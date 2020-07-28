5ebec0d20782cf0889e52783946ebb889ba6275c
Nicolas Toporcov <ntoporcov@me.com>
2020-07-27 20:00:29 -0400

    Updates

---
+++ precache-manifest.a18122f0aa55ac8e4bc6a954a8e45460.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1433c58a1995a1d50e55e2256682f8d7",
+    "url": "/bravia-shortcuts/index.html"
+  },
+  {
+    "revision": "d41a4d06ffe7b3e33df8",
+    "url": "/bravia-shortcuts/static/css/main.3dd75c4c.chunk.css"
+  },
+  {
+    "revision": "0927e1cf3735c5ab351a",
+    "url": "/bravia-shortcuts/static/js/2.1b252091.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bravia-shortcuts/static/js/2.1b252091.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d41a4d06ffe7b3e33df8",
+    "url": "/bravia-shortcuts/static/js/main.946ac000.chunk.js"
+  },
+  {
+    "revision": "7b49c7e8f2c71c95026d",
+    "url": "/bravia-shortcuts/static/js/runtime-main.2c627342.js"
+  },
+  {
+    "revision": "3d9c9bb2393b0f1eb5dbeaf3bad9ad06",
+    "url": "/bravia-shortcuts/static/media/helper.3d9c9bb2.png"
+  },
+  {
+    "revision": "cf75d966a42efe2e430db6c641c773e3",
+    "url": "/bravia-shortcuts/static/media/icons.cf75d966.png"
+  },
+  {
+    "revision": "0b09c3bd4a89f969bb9dadac9d6d635d",
+    "url": "/bravia-shortcuts/static/media/power.0b09c3bd.png"
+  },
+  {
+    "revision": "7555cc06bdbd93c50a71e290f679c39c",
+    "url": "/bravia-shortcuts/static/media/setup.7555cc06.png"
+  }
+]);
